42f06399b1e219d385c12dbd9747bec42e8a1d04
JulienC <37269289+JulienCorbo@users.noreply.github.com>
2022-04-29 15:54:14 +0200

    :sparkles: Ajout classe character & monster

---
+++ models/entity/characters/character.php
@@ -0,0 +1,55 @@
+<?php
+
+Class Character {
+    Private $life;
+    Private $damage;
+    Private $name;
+
+    /**
+     * @return mixed
+     */
+    public function getLife()
+    {
+        return $this->life;
+    }
+
+    /**
+     * @param mixed $life
+     */
+    public function setLife($life)
+    {
+        $this->life = $life;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDamage()
+    {
+        return $this->damage;
+    }
+
+    /**
+     * @param mixed $damage
+     */
+    public function setDamage($damage)
+    {
+        $this->damage = $damage;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
---
+++ models/entity/characters/monster.php
@@ -0,0 +1,52 @@
+<?php
+
+Class Monster {
+    Private $life;
+    Private $damage;
+    Private $name;
+
+    public function getLife()
+    {
+        return $this->life;
+    }
+
+    /**
+     * @param mixed $life
+     */
+    public function setLife($life)
+    {
+        $this->life = $life;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDamage()
+    {
+        return $this->damage;
+    }
+
+    /**
+     * @param mixed $damage
+     */
+    public function setDamage($damage)
+    {
+        $this->damage = $damage;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
